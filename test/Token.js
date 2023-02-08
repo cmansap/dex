9b0d0a43ebd15c98fb533a163e2bcfdb1035837e
Chandra Mannava <cmansap@gmail.com>
2023-02-08 09:00:01 -0600

    dex setup1

---
+++ test/Token.js
@@ -0,0 +1,40 @@
+const { expect } = require("chai");
+const {ethers} = require("hardhat");
+
+describe('Token Contract',async()=>{
+
+    const tokens = (n)=>{
+        return ethers.utils.parseUnits(n.toString(),'ether')
+    }
+    let token
+    beforeEach(async()=>{
+        const Token = await ethers.getContractFactory('Token')
+        token = await Token.deploy('JANASENA','JSP','1000000')
+        await token.deployed()
+    })
+
+    describe("Deployment",()=>{
+        const name = 'JANASENA'
+        const symbol = 'JSP'
+        const decimals = '18'
+        const totalSupply = tokens(1000000)
+        it('has a correct name',async()=>{
+            expect(await token.name()).to.equal(name)
+        })
+    
+        it('has a correct symbol',async()=>{
+            expect(await token.symbol()).to.equal(symbol)
+        })
+    
+        it('has a correct decimals',async()=>{
+            expect(await token.decimals()).to.equal(18)
+        })
+    
+        it('has a correct totalSupply',async()=>{
+            expect(await token.totalSupply()).to.equal(totalSupply)
+        })
+    })  
+})
+
+
+
